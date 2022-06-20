c6845a9d484a50eab1e0be3fcc706bdf26630233
Sahadporn Charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2022-06-20 16:19:17 +0700

    add cors

--- index.ts
+++ index.ts
@@ -1,17 +1,24 @@
+import cors from "cors"
 import express from "express"
 import dotenv from "dotenv"
 import swaggerUi from "swagger-ui-express"
 import * as swaggerDocument from "./swagger.json"
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 import bodyParser from "body-parser"
 ;-bodyParser
 
 dotenv.config()
 
 const app = express()
-const port = /*process.env.PORT ||*/ 3000
-const uri = /*process.env.MONGO_URI ||*/ "mongodb://root:root@localhost:27017/"
+const port = process.env.PORT || 3000
+const uri = process.env.MONGO_URI || "mongodb://root:root@localhost:27017/"
 
+const allowedOrigins = ['http://localhost:3000']
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+}
+
+app.use(cors(options))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -23,6 +30,30 @@ const client = new MongoClient(uri)
 }
 const db = client.db("student")
 const profile = db.collection("profile")
+
+interface Profile {
+  _id: ObjectId,
+  name: string,
+  age: number,
+  address: string[],
+  created_date: Date,
+  updated_date: Date,
+}
+
+function convertToObject(body: any): Profile {
+  return {
+    _id: <ObjectId> body._id,
+    name: <string> body.name,
+    age: <number> body.age,
+    address: <string[]> body.address,
+    created_date: new Date(),
+    updated_date: new Date(),
+  }
+}
+
+app.get("/", async (req: express.Request, res) => {
+    res.status(200).send("Welcome please go to /swagger")
+})
 
 // GET all student data
 app.get("/student", async (req: express.Request, res) => {
@@ -52,14 +83,8 @@ app.post("/student", async (req: express.Request, res: express.Response) => {
     if (Object.keys(req.body).length <= 0) {
       res.status(500).send("No input data")
     } else {
-      await profile.insertOne({
-        _id: req.body._id,
-        name: req.body.name,
-        age: req.body.age,
-        address: req.body.address,
-        created_date: new Date(),
-        updated_date: new Date(),
-      })
+      let data: Profile = convertToObject(req.body)
+      await profile.insertOne(data)
     }
 
     res.status(201).send({ status: "SUCCESS" })
