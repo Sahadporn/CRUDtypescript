9e742e0617128da0e60e491790beeca8bab831cd
army2102 <naetirat.s@gmail.com>
2022-08-15 17:47:28 +0700

    Add TODOs

--- presenters/index.ts
+++ presenters/index.ts
@@ -1,52 +1,65 @@
-import "reflect-metadata"
+import 'reflect-metadata'
 
-import express, { NextFunction } from "express"
-import cors from "cors"
-import { config } from "./config"
-import bodyParser from "body-parser"
-import methodOverride from "method-override"
+import express, { NextFunction } from 'express'
+import cors from 'cors'
+import { config } from './config'
+import bodyParser from 'body-parser'
+import methodOverride from 'method-override'
 import Joi, { string } from 'joi'
 
-import { profileRoute } from "./routes/profile-routes"
-import swaggerUi from "swagger-ui-express"
-import * as swaggerDocument from "../swagger.json"
-import { InitContainer } from "./di"
+import { profileRoute } from './routes/profile-routes'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from '../swagger.json'
+import { InitContainer } from './di'
 
-
-
-function errorHandler (err: Error, req: express.Request, res: express.Response, next: NextFunction) {
+function errorHandler(
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  next: NextFunction
+) {
   res.status(500)
   res.render('error ', { error: err.message })
   next()
 }
 
+// TODO: Refactor, this function should only contain API Server creation logic
+//
+// move  InitContainer & app.listen to outside
+// Rename initApp to createServer
 
 const initApp = async () => {
   await InitContainer()
 
   const app = express()
-  const allowedOrigins = ["http://localhost:3000"]
+  // TODO: Move middleware together
+
+  // TODO: Using lib would be better
+  // https://expressjs.com/en/resources/middleware/cors.html
+  const allowedOrigins = ['http://localhost:3000']
   const options: cors.CorsOptions = {
-    origin: allowedOrigins,
+    origin: allowedOrigins
   }
 
-  
   app.use(cors(options))
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
-  app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-  app.get("/", async (req: express.Request, res) => {
+  // TODO: Move to routes (./routes/index.ts)
+  app.get('/', async (req: express.Request, res) => {
     res
       .status(200)
       .send(
-        "This is the clean-architecture-implemented version. Go to /swagger for doc."
+        'This is the clean-architecture-implemented version. Go to /swagger for doc.'
       )
   })
 
-  app.use("/student", profileRoute)
+  // TODO: change to /profile
+  app.use('/profile', profileRoute)
 
   app.use(methodOverride())
+  // TODO: Just implement directly, too little code to move to another function
   app.use(errorHandler)
 
   app.listen(config.port, () =>
