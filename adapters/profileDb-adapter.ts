96f1006ffb80a763516e05a59908d758f9cf95f5
Sahadporn Charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2022-08-18 11:45:03 +0700

    edit update in db adapter

--- adapters/profileDb-adapter.ts
+++ adapters/profileDb-adapter.ts
@@ -1,50 +1,50 @@
-import { Collection, MongoClient, ObjectId } from 'mongodb'
-import { ProfileEntity } from '../entity/Profile-entity'
-import { ProfileDbInterface } from '../useCases/profile-usecases'
-import { Service } from 'typedi'
+import { Collection, MongoClient, ObjectId } from "mongodb";
+import { ProfileEntity } from "../entities/Profile-entity";
+import { ProfileDbInterface } from "../useCases/profile-usecases";
+import { Service } from "typedi";
 
 type DbConfig = {
-  user: string
-  password: string
-  host: string
-  port: number
-  db: string
-  collection: string
-}
+  user: string;
+  password: string;
+  host: string;
+  port: number;
+  db: string;
+  collection: string;
+};
 
 @Service()
 export class ProfileDbAdapter implements ProfileDbInterface {
-  private dbCollection?: Collection<ProfileEntity>
-  private readonly dbConfig: DbConfig
+  private dbCollection?: Collection<ProfileEntity>;
+  private readonly dbConfig: DbConfig;
 
   constructor(dbConfig: DbConfig) {
-    this.dbConfig = dbConfig
+    this.dbConfig = dbConfig;
   }
 
   public async connect(): Promise<void> {
-    let uri = `mongodb://${this.dbConfig.user}:${this.dbConfig.password}@${this.dbConfig.host}:${this.dbConfig.port}/`
+    let uri = `mongodb://${this.dbConfig.user}:${this.dbConfig.password}@${this.dbConfig.host}:${this.dbConfig.port}/`;
 
-    const connection = await new MongoClient(uri).connect()
+    const connection = await new MongoClient(uri).connect();
     this.dbCollection = connection
       .db(this.dbConfig.db)
-      .collection(this.dbConfig.collection)
+      .collection(this.dbConfig.collection);
   }
 
   public getConfig() {
-    return this.dbConfig
+    return this.dbConfig;
   }
 
   public async getAll(): Promise<ProfileEntity[]> {
     const data = await (this.dbCollection as Collection<ProfileEntity>)
       .find()
-      .toArray()
+      .toArray();
 
     if (!data) {
-      throw new Error(`Cannot retrieve data`)
+      throw new Error(`Cannot retrieve data`);
     }
 
-    let arr: ProfileEntity[] = []
-    data.map(element => {
+    let arr: ProfileEntity[] = [];
+    data.map((element) => {
       arr.push(
         new ProfileEntity(
           element.name,
@@ -54,18 +54,18 @@ export class ProfileDbAdapter implements ProfileDbInterface {
           element.updated_date,
           element._id.toString()
         )
-      )
-    })
-    return arr
+      );
+    });
+    return arr;
   }
 
   public async getById(id: string): Promise<ProfileEntity> {
     let data = await (this.dbCollection as Collection<ProfileEntity>).findOne({
-      _id: new ObjectId(id)
-    })
+      _id: new ObjectId(id),
+    });
 
     if (!data) {
-      throw new Error(`Cannot find profile with id ${id}}`)
+      throw new Error(`Cannot find profile with id ${id}}`);
     }
 
     return new ProfileEntity(
@@ -75,7 +75,7 @@ export class ProfileDbAdapter implements ProfileDbInterface {
       data.created_date,
       data.updated_date,
       data._id as unknown as string
-    )
+    );
   }
 
   public async insert(profile: ProfileEntity): Promise<void> {
@@ -87,30 +87,30 @@ export class ProfileDbAdapter implements ProfileDbInterface {
         age: profile.age,
         address: profile.address,
         updated_date: new Date(),
-        created_date: new Date()
-      })
+        created_date: new Date(),
+      });
     } catch (err) {
-      console.log('Insert failed: ', err)
+      console.log("Insert failed: ", err);
     }
   }
 
-  public async update(id: string, update: ProfileEntity): Promise<void> {
+  public async update(update: ProfileEntity): Promise<void> {
     try {
       let res = await (
         this.dbCollection as Collection<ProfileEntity>
       ).updateOne(
-        { _id: new ObjectId(id) },
+        { _id: new ObjectId(update.id) },
         {
           $currentDate: { updated_date: true },
           $set: {
             name: update.name,
             age: update.age,
-            address: update.address
-          }
+            address: update.address,
+          },
         }
-      )
+      );
     } catch (err) {
-      console.log('Update failed: ', err)
+      console.log("Update failed: ", err);
     }
   }
 
@@ -118,9 +118,9 @@ export class ProfileDbAdapter implements ProfileDbInterface {
     try {
       let res = await (
         this.dbCollection as Collection<ProfileEntity>
-      ).deleteOne({ _id: new ObjectId(id) })
+      ).deleteOne({ _id: new ObjectId(id) });
     } catch (err) {
-      console.log('Deletion failed: ', err)
+      console.log("Deletion failed: ", err);
     }
   }
 }
--- useCases/profile-usecases.ts
+++ useCases/profile-usecases.ts
@@ -1,61 +1,59 @@
-import { ProfileEntity } from '../entity/Profile-entity'
-import { Container, Inject, Service } from 'typedi'
-import { ProfileDbAdapter } from '../adapters/profileDb-adapter'
+import { ProfileEntity } from "../entities/Profile-entity";
+import { Container, Inject, Service } from "typedi";
+import { ProfileDbAdapter } from "../adapters/profileDb-adapter";
 export interface ProfileDbInterface {
-  getAll(): Promise<ProfileEntity[]>
-  getById(id: string): Promise<ProfileEntity>
-  insert(profile: ProfileEntity): Promise<void>
-  update(id: string, update: ProfileEntity): Promise<void>
-  delete(id: string): Promise<void>
+  getAll(): Promise<ProfileEntity[]>;
+  getById(id: string): Promise<ProfileEntity>;
+  insert(profile: ProfileEntity): Promise<void>;
+  update(update: ProfileEntity): Promise<void>;
+  delete(id: string): Promise<void>;
 }
 
 @Service()
 export class ProfileUseCase {
   constructor(
-    @Inject('ProfileDbDi')
+    @Inject("ProfileDbDi")
     private readonly profileDb: ProfileDbAdapter
   ) {
-    this.profileDb.connect()
+    this.profileDb.connect();
   }
 
   public async getAllProfiles(): Promise<ProfileEntity[]> {
-    return await this.profileDb.getAll()
+    return await this.profileDb.getAll();
   }
 
   public async getProfileById(id: string): Promise<ProfileEntity> {
-    return await this.profileDb.getById(id)
+    return await this.profileDb.getById(id);
   }
 
   public async createProfile(name: string, age: number, address: string[]) {
-    let profile = new ProfileEntity(name, age, address, new Date(), new Date())
-    return await this.profileDb.insert(profile)
+    let profile = new ProfileEntity(name, age, address, new Date(), new Date());
+    return await this.profileDb.insert(profile);
   }
 
   public async updateProfile(
     id: string,
     update: Partial<{
-      name: string
-      age: number
-      address: string[]
+      name: string;
+      age: number;
+      address: string[];
     }>
   ) {
-    const profile = await this.profileDb.getById(id)
-    for (let element in update) {
+    const profile = await this.profileDb.getById(id);
+    for (const element in update) {
       if (element !== undefined) {
-        let key = element as keyof {
-          name: string
-          age: number
-          address: string[]
-        }
-        ;(profile[key] as any) = update[key]
+        const key = element as keyof Pick<
+          ProfileEntity,
+          "name" | "age" | "address"
+        >;
+        (profile[key] as any) = update[key];
       }
     }
 
-    // TODO: Get id from ProfileEntity
-    return await this.profileDb.update(id, profile)
+    return await this.profileDb.update(profile);
   }
 
   public async deleteProfile(id: string) {
-    return await this.profileDb.delete(id)
+    return await this.profileDb.delete(id);
   }
 }
