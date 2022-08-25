ea94732af09db63e546004ed2b2627e586eb1f84
Sahadporn Charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2022-08-25 15:15:42 +0700

    fix joi middleware

--- presenters/routes/profile-routes.ts
+++ presenters/routes/profile-routes.ts
@@ -1,11 +1,9 @@
 import express, { NextFunction } from "express";
-// import Joi from "joi"
 import { Container } from "typedi";
 import { ProfileEntity } from "../../entities/Profile-entity";
+import Joi from "joi";
 
 import { ProfileUseCase } from "../../useCases/profile-usecases";
-
-const Joi = require("joi");
 
 const inputSchema = Joi.object({
   name: Joi.string().min(2).max(30),
@@ -13,41 +11,22 @@ const inputSchema = Joi.object({
   address: Joi.array().items(Joi.string()),
 });
 
-// const middleware = (schema: any) => {
-//   return (req: express.Request, res: express.Response, next: NextFunction) => {
-//   const { error } = Joi.validate(req.body, schema);
-//   const valid = error == null;
+const middleware = (schema: Joi.ObjectSchema) => {
+  return (req: express.Request, res: express.Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    const valid = error == null;
 
-//   if (valid) {
-//     next();
-//   } else {
-//     const { details } = error;
-//     const message = details.map(i => i.message).join(',');
+    if (valid) {
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(",");
 
-//     console.log("error", message);
-//    res.status(422).json({ error: message }) }
-//   }
-// }
-
-// joiValidator runnable but not working
-// post method does not call joi middleware
-function joiValidator(
-  err: Error,
-  req: express.Request,
-  res: express.Response,
-  next: NextFunction
-) {
-  console.log("inside joiValidator");
-  if (Object.keys(req.body).length <= 0) {
-    res.status(500).send("No input data");
-  }
-  const { name, age, address } = req.body;
-  if (Joi.validate({ name, age, address }, inputSchema)) {
-    next();
-  } else {
-    res.status(500).send("Incorrect input data");
-  }
-}
+      console.log("error", message);
+      res.status(422).json({ error: message });
+    }
+  };
+};
 
 function profileFormatter(profile: ProfileEntity) {
   return {
@@ -124,6 +103,6 @@ export const profileRoute = express.Router();
 
 profileRoute.get("/", getAllProfileController);
 profileRoute.get("/:id", getProfileController);
-profileRoute.post("/", createProfileController);
-profileRoute.put("/:id", updateProfileController);
+profileRoute.post("/", middleware(inputSchema), createProfileController);
+profileRoute.put("/:id", middleware(inputSchema), updateProfileController);
 profileRoute.delete("/:id", deleteProfileController);
